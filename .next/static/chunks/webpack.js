70a35b8fe4f336643680f1b92a5d8e767af39fee
Nicol-SX <nicol.lee@swiftx.co>
2025-06-10 18:10:14 +0800

    debug comment column

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b9afb3f410a28ad"; }
+/******/ 		__webpack_require__.h = function() { return "de4b7060a412657d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
