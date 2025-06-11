28121eaa7133dff4563fd8f77a14e7ffa7e0644e
SX-Melvin <melvin@swiftx.co>
2025-06-11 14:23:21 +0700

    feat: convert to ISO local tz

---
+++ app/common/to-local-iso-string.ts
@@ -0,0 +1,6 @@
+export default function toLocalISOString(date: Date): string {
+  const pad = (n: number) => n.toString().padStart(2, '0');
+
+  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T` +
+         `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
+}
